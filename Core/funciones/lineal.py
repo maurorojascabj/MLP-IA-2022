4813c666ba3bae088312426fb3a63164361f9741
VictoriaQuetglas <vickyvistola95@gmail.com>
2022-10-30 17:19:46 -0300

    funciones

--- Core/funciones/lineal.py
+++ Core/funciones/lineal.py
@@ -2,5 +2,5 @@
 from core.funciones.interfaz_funcion import interfaz_funcion
 
 class lineal(interfaz_funcion):
-    def calcular(self,net):
-        return 2
+    def calcular(self, net):
+        return net
--- Core/funciones/sigmoidal.py
+++ Core/funciones/sigmoidal.py
@@ -1,8 +1,8 @@
 
 
+import math
 from core.funciones.interfaz_funcion import interfaz_funcion
-
 
 class sigmoidal(interfaz_funcion): 
     def calcular(self,net):
-        return 2   
+        return 1 / (1+(math.e**(-net)))  
